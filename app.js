5cffe42610dea32be1fbd31412c576d2fc211e10
DanielDinchew <108183002+DanielDinchew@users.noreply.github.com>
2024-04-01 00:58:34 +0300

    Full chat

--- app.js
+++ app.js
@@ -21,4 +21,13 @@ function onConnection(socket) {
         socketsConnected.delete(socket.id)
         io.emit('clients-total', socketsConnected.size)
     }) 
+
+    socket.on('message', (data) => {
+        // console.log(data)
+        socket.broadcast.emit('chat-message', data)
+      })
+
+      socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
+      })
 }
