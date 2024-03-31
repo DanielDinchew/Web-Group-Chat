69b6ee53084fea0b432b4f57ed8a9a243677524f
Alex <alexpalex.ah@gmail.com>
2024-03-31 20:17:36 +0300

    created server-side socket which emits Total Clients number

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const exress = require('express')
+const express = require('express')
 const path = require('path')
-const app = exress()
+const app = express()
 const PORT = 4000
 const server = app.listen(PORT, () => console.log('chat server on port 4000'))
 
-app.use(exress.static(path.join(__dirname, 'public')))
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConnected = new Set()
+
+io.on('connection', onConnection)
+
+function onConnection(socket) {
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total', socketsConnected.size)
+
+    socket.on('disconnect', () => {
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    }) 
+}
